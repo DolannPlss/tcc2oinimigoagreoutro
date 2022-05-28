68d59010e0e14d7892c7086e88d9e666ffac7177
luizosb <luiz-osb@hotmail.com>
2022-05-28 17:17:51 -0300

    agora vai

--- src/main/java/com/projeto/barganhaleilao/repository/Produtos.java
+++ src/main/java/com/projeto/barganhaleilao/repository/Produtos.java
@@ -4,15 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.projeto.barganhaleilao.model.CadProduto;
 
 public interface Produtos extends JpaRepository<CadProduto, Long>{
 	
 	@Query(nativeQuery = true, value = "SELECT cli.celular FROM clientes cli, "
-			+ "cad_produto prod WHERE cli.usuario = :cliente and cli.usuario = prod.usuario")
-	String buscarCelularDoVendedor(@Param("cliente") String cliente);
+			+ "cad_produto prod WHERE cli.usuario = ?1 and cli.usuario = prod.usuario")
+	public String buscarCelularDoVendedor(String cliente);
 	
 	public List<CadProduto> findByProdutoContaining(String produto);
 	
--- src/main/java/com/projeto/barganhaleilao/repository/UsuarioInterface.java
+++ src/main/java/com/projeto/barganhaleilao/repository/UsuarioInterface.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.projeto.barganhaleilao.model.Clientes;
 
 public interface UsuarioInterface extends JpaRepository<Clientes, Long>{
-    @Query("SELECT u FROM Clientes u WHERE u.usuario = ?1")
+    
+	@Query("SELECT u FROM Clientes u WHERE u.usuario = ?1")
     public Clientes findByUsuario(String usuario);
 }
