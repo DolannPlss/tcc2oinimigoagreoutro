f399eb2b56658fa444f01ce932e1606115e3518d
luizosb <luiz-osb@hotmail.com>
2022-05-28 17:07:25 -0300

    sql arrumandoo

--- src/main/java/com/projeto/barganhaleilao/repository/Produtos.java
+++ src/main/java/com/projeto/barganhaleilao/repository/Produtos.java
@@ -2,8 +2,6 @@ package com.projeto.barganhaleilao.repository;
 
 import java.util.List;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +10,8 @@ import com.projeto.barganhaleilao.model.CadProduto;
 
 public interface Produtos extends JpaRepository<CadProduto, Long>{
 	
-	@Query(nativeQuery = true, value = "SELECT cli.celular FROM {h-schema}clientes cli, "
-			+ "{h-schema}cad_produto prod WHERE cli.usuario = :cliente and cli.usuario = prod.usuario")
+	@Query(nativeQuery = true, value = "SELECT cli.celular FROM clientes cli, "
+			+ "cad_produto prod WHERE cli.usuario = :cliente and cli.usuario = prod.usuario")
 	String buscarCelularDoVendedor(@Param("cliente") String cliente);
 	
 	public List<CadProduto> findByProdutoContaining(String produto);
